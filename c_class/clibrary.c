a8eb91c3a3e8985b38ce4b25d47c85c55f86f5a8
Cgyc20 <cgyc20@bath.ac.uk>
2024-11-21 14:56:42 +0000

    optimised

--- c_class/clibrary.c
+++ c_class/clibrary.c
@@ -62,40 +62,54 @@ void CalculatePropensity(int SSA_M, float *PDE_list, int *SSA_list, float *prope
                          float *combined_mass_list, float *Approximate_PDE_Mass, 
                          int *boolean_mass_list, float degradation_rate, float threshold, 
                          float Production_rate_PC, float gamma, float jump_rate) {
-    
+    // Precompute commonly used values
+    int two_SSA_M = 2 * SSA_M;
+    int three_SSA_M = 3 * SSA_M;
+    int four_SSA_M = 4 * SSA_M;
+    int five_SSA_M = 5 * SSA_M;
+
     // Initialize all propensity values to zero
-    for (int i = 0; i < 5 * SSA_M; i++) {
+    for (int i = 0; i < five_SSA_M; i++) {
         propensity_list[i] = 0.0f;
     }
 
     // Diffusion propensities (boundary conditions)
-    propensity_list[0] = jump_rate * (float)SSA_list[0];
+    float jump_rate_f = jump_rate;
+    propensity_list[0] = jump_rate_f * SSA_list[0];
     for (int i = 1; i < SSA_M - 1; i++) {
-        propensity_list[i] = 2 * jump_rate * (float)SSA_list[i];
+        propensity_list[i] = 2.0f * jump_rate_f * SSA_list[i];
     }
-    propensity_list[SSA_M - 1] = jump_rate * (float)SSA_list[SSA_M - 1];
+    propensity_list[SSA_M - 1] = jump_rate_f * SSA_list[SSA_M - 1];
 
     // Production rates (constant for each compartment)
-    for (int i = SSA_M; i < 2 * SSA_M; i++) {
+    for (int i = SSA_M; i < two_SSA_M; i++) {
         propensity_list[i] = Production_rate_PC;
     }
 
     // Degradation rates (depends on SSA_list)
-    for (int i = 2 * SSA_M; i < 3 * SSA_M; i++) {
-        propensity_list[i] = degradation_rate * (float)SSA_list[i - 2 * SSA_M];
+    float degradation_rate_f = degradation_rate;
+    for (int i = two_SSA_M; i < three_SSA_M; i++) {
+        propensity_list[i] = degradation_rate_f * SSA_list[i - two_SSA_M];
     }
 
     // Conversion from continuous to discrete (below threshold)
-    for (int i = 3 * SSA_M; i < 4 * SSA_M; i++) {
-        if (combined_mass_list[i - 3 * SSA_M] < threshold) {
-            propensity_list[i] = gamma * Approximate_PDE_Mass[i - 3 * SSA_M] * (float)boolean_mass_list[i - 3 * SSA_M];
-        }
+    float threshold_f = threshold;
+    float gamma_f = gamma;
+    for (int i = three_SSA_M; i < four_SSA_M; i++) {
+        float combined_mass = combined_mass_list[i - three_SSA_M];
+        float approx_mass = Approximate_PDE_Mass[i - three_SSA_M];
+        int boolean_mass = boolean_mass_list[i - three_SSA_M];
+        propensity_list[i] = (combined_mass < threshold_f) 
+                              ? gamma_f * approx_mass * boolean_mass 
+                              : 0.0f;
     }
 
     // Conversion from discrete to continuous (above threshold)
-    for (int i = 4 * SSA_M; i < 5 * SSA_M; i++) {
-        if (combined_mass_list[i - 4 * SSA_M] >= threshold) {
-            propensity_list[i] = gamma * (float)SSA_list[i - 4 * SSA_M];
-        }
+    for (int i = four_SSA_M; i < five_SSA_M; i++) {
+        float combined_mass = combined_mass_list[i - four_SSA_M];
+        int SSA_mass = SSA_list[i - four_SSA_M];
+        propensity_list[i] = (combined_mass >= threshold_f) 
+                              ? gamma_f * SSA_mass 
+                              : 0.0f;
     }
 }
