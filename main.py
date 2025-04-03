a2c616e85e4804cf4ccc43a38ff5d331398997ee
Cgyc20 <cgyc20@bath.ac.uk>
2025-04-03 09:33:15 +0100

    Comment out simulation runs and SSA output in main function

--- main.py
+++ main.py
@@ -60,8 +60,8 @@ def main():
 
     Model = Hybrid(domain_length, compartment_number, PDE_multiple, total_time, timestep, particles_per_compartment_thresh, gamma, production_rate, degradation_rate, diffusion_rate, SSA_initial, use_c_functions=False) # Define the hybrid model
 
-    Hybrid_SSA, Hybrid_PDE, Hybrid_combined = Model.run_simulation(number_of_repeats=repeats)
-    Model.save_simulation_data(Hybrid_SSA, Hybrid_PDE, Hybrid_combined, datadirectory='data')
+    #Hybrid_SSA, Hybrid_PDE, Hybrid_combined = Model.run_simulation(number_of_repeats=repeats)
+    #Model.save_simulation_data(Hybrid_SSA, Hybrid_PDE, Hybrid_combined, datadirectory='data')
 
     SSA_model = Stochastic(domain_length, compartment_number, total_time, timestep, production_rate, degradation_rate, diffusion_rate, SSA_initial, use_c_functions=False)
     SSA_grid = SSA_model.run_simulation(number_of_repeats=repeats)
@@ -77,7 +77,7 @@ def main():
 
     print(f"PDE grid at timestep one: {PDE_grid[:,0]}")
     # print(f"Stochastic grid at timestep 1: {SSA_grid[:,0]}")
-    print(f"SSA in hybrid model at timestep 1: {Hybrid_SSA[:,0]}")
+    #print(f"SSA in hybrid model at timestep 1: {Hybrid_SSA[:,0]}")
 
 def plot():
     subprocess.run(['python', 'animate.py'])
