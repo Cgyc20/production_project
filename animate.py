d1d56d32a7fca2a8de574e8aa0ec08f253299638
Cgyc20 <cgyc20@bath.ac.uk>
2025-04-28 11:39:00 +0100

    Reduce animation interval in FuncAnimation for smoother playback

--- animate.py
+++ animate.py
@@ -145,7 +145,7 @@ def main():
         return (*bar_SSA, *bar_pure_SSA, line_combined, line_PDE, line_pure_PDE, time_text, threshold_line, steady_state_line)
 
     # Create animation
-    ani = FuncAnimation(fig, update, frames=range(0, len(time_vector), 1), interval=40)
+    ani = FuncAnimation(fig, update, frames=range(0, len(time_vector), 1), interval=1)
 
     # Set legend position fixed
     # Adjust the figure layout to make space for the legend
